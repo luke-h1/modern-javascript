60d7df14504e2a287828231b9d15d5e765ba2e7f
luke-h1 <luke.howsam@yahoo.com>
2020-07-07 12:21:14 +0100

    strings and variables

---
+++ section-3-variables-flow-controls/strings-and-variables/app.js
@@ -0,0 +1,12 @@
+// lesson content 
+let fname = 'Luke'; 
+let lname = 'last_name';
+let fullName = fname + ' ' + lname; 
+console.log(fullName); 
+
+
+// challenge 
+let city = 'london'; 
+let country = 'united kingdon' ; 
+let location = city + ',' + ' ' +  country  ; 
+console.log(location); 
