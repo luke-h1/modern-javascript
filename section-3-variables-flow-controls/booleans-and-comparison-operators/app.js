83a5f4226f70e2b0859c0cd59c5955a7f18e5dce
luke-h1 <luke.howsam@yahoo.com>
2020-07-07 15:23:38 +0100

    update

---
+++ section-3-variables-flow-controls/booleans-and-comparison-operators/app.js
@@ -0,0 +1,9 @@
+let temp = 31 ; 
+let isFreezing = temp !== 31  ; 
+console.log(isFreezing) ; 
+
+
+// === - equality operator 
+// !== - not equal operator 
+// < - less than operator 
+// > - greater than operator 
