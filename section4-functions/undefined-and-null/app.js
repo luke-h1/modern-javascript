d01270f693a4108532cc516d1f689c9e55829010
luke-h1 <luke.howsam@yahoo.com>
2020-07-15 11:23:09 +0100

    update

---
+++ section4-functions/undefined-and-null/app.js
@@ -0,0 +1,24 @@
+let name ; 
+name = 'Luke' ; 
+
+
+if(name === undefined) { 
+    console.log('please enter a value')
+}else { 
+    console.log('value has been entered')
+}; 
+console.log(name)
+
+
+let square = function(num) { 
+    console.log(num) 
+}; 
+
+// Null as assigned value (explicity set) 
+// undefined is a language feature of JS 
+
+let age = '21' ; 
+
+age = null; 
+
+console.log(age); 
