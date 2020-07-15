421f784b139ebf01eb00b955e542d6cb90839496
luke-h1 <luke.howsam@yahoo.com>
2020-07-15 19:49:34 +0100

    update

--- section5-objects/methods/app.js
+++ section5-objects/methods/app.js
@@ -15,3 +15,9 @@ let restaurant = {
         this.guestCount = this.guestCount - partySize 
     }
 }; 
+
+
+restaurant.seatParty(72);  // seats 72 people 
+console.log(restaurant.checkAvailability(4));  // return false as capacity is 75.  
+restaurant.removeParty(5); // removes 5 people 
+console.log(restaurant.checkAvailability(4)); //return true as there is enough space for 5 
