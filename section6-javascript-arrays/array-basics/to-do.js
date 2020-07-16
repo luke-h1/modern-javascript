7dee223923f1912af010395b3569b4d98abe07c8
luke-h1 <luke.howsam@yahoo.com>
2020-07-16 15:24:52 +0100

    update

---
+++ section6-javascript-arrays/array-basics/to-do.js
@@ -0,0 +1,13 @@
+const toDo = [
+    'go for a walk',
+    'pay the bills',
+    'learn javascript',
+    'drink enough water',
+    'review PRs',
+];
+console.log(`You have ${toDo.length } things to do`); 
+console.log(`To-do : ${toDo[0]}`) 
+console.log(`To-do: ${toDo[toDo.length -2]}`)
+
+
+
