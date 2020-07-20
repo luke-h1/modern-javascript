29117e3136580687b79e736f62e6fe1218297c5b
luke-h1 <luke.howsam@yahoo.com>
2020-07-20 16:37:01 +0100

    improve expense tracker

--- section6-javascript-arrays/improve-expense-tracker/app.js
+++ section6-javascript-arrays/improve-expense-tracker/app.js
@@ -1,24 +1,39 @@
 const account = {
     name: 'Luke',
     expenses: [],
+    income: [],
     addExpense: function (description, amount) {
         this.expenses.push({
             description: description,
             amount: amount,
-        })
+        });
     },
+
+    addIncome: function (description, amount) {
+        this.income.push({
+            description: description,
+            amount: amount,
+        });
+    },
+
     getAccountSummary: function () {
         let totalExpenses = 0;
+        let totalIncome = 0;
+        let accountBalance = 0;
         this.expenses.forEach(function (expense) {
             totalExpenses = totalExpenses + expense.amount;
-        })
+        });
+        this.income.forEach(function (income) {
+            totalIncome = totalIncome + income.amount;
+        });
 
-        return `${this.name} has £${totalExpenses}.00 in expenses.`;
-    } 
-} 
-
+        accountBalance = totalIncome - totalExpenses;
+        return `${this.name} has a balance of £${accountBalance}, £${totalIncome} in income. £${totalExpenses} in expenses`
+    },
+};
 account.addExpense('coffee', 2);
 account.addExpense('rent', 950);
+account.addIncome('job', 1000);
+console.log(account.getAccountSummary());
 
 
-console.log(account.getAccountSummary())
